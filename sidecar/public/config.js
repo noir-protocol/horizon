275e127c895af8636cdd47171220534673fb3b1a
code0xff <ian.jungyong.um@gmail.com>
2023-08-17 16:06:14 +0900

    feat: Add cosmos api sidecar

---
+++ sidecar/public/config.js
@@ -0,0 +1,16 @@
+const config = {
+	chainId: "dev",
+	chainName: "Horizon",
+	addressPrefix: "cosmos",
+	baseDenom: "acdt", // atto cdt
+	displayDenom: "CDT",
+	restURL: "http://127.0.0.1:1317",
+	coinType: "118",
+	decimals: 18,
+	gasRate: {
+		average: "1",
+		low: "0.8",
+		tiny: "0.6",
+	},
+	sendGas: "1000000000"
+};
