b2e9d0d4fa61b258bf5351f90d7afd5c11c6cd87
Jeeyong Um <conr2d@proton.me>
2023-08-25 02:11:18 +0900

    chore: Change template license to Unlicense

--- template/node/src/service.rs
+++ template/node/src/service.rs
@@ -17,7 +17,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use futures::prelude::*;
-use horizon_runtime::{self, opaque::Block, RuntimeApi};
+use horizon_template_runtime::{self, opaque::Block, RuntimeApi};
 use sc_client_api::{Backend, BlockBackend};
 use sc_consensus_aura::{ImportQueueParams, SlotProportion, StartAuraParams};
 use sc_consensus_grandpa::SharedVoterState;
@@ -35,11 +35,11 @@ impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
 	type ExtendHostFunctions = hp_io::crypto::HostFunctions;
 
 	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
-		horizon_runtime::api::dispatch(method, data)
+		horizon_template_runtime::api::dispatch(method, data)
 	}
 
 	fn native_version() -> sc_executor::NativeVersion {
-		horizon_runtime::native_version()
+		horizon_template_runtime::native_version()
 	}
 }
 
