4086964abdd6ff7f8324e302cd0a505bc79b3d9e
Jungyong Um <ian@haderech.co>
2024-06-18 12:42:33 +0800

    refactor: Simplify code

--- frame/cosmos/decorators/auth/src/sigverify.rs
+++ frame/cosmos/decorators/auth/src/sigverify.rs
@@ -17,6 +17,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use hp_cosmos::{PublicKey, SignerPublicKey, Tx};
+use hp_io::crypto::secp256k1_ecdsa_verify;
 use pallet_cosmos_decorators::AnteDecorator;
 use sp_runtime::transaction_validity::{InvalidTransaction, TransactionValidityError};
 use sp_std::marker::PhantomData;
@@ -49,7 +50,7 @@ where
 			if let SignerPublicKey::Single(PublicKey::Secp256k1(public_key)) = public_key {
 				// TODO: Validate if the sequence of the account is valid
 
-				if !hp_io::crypto::secp256k1_ecdsa_verify(sig, &tx.hash.0, public_key) {
+				if !secp256k1_ecdsa_verify(sig, &tx.hash.0, public_key) {
 					return Err(TransactionValidityError::Invalid(InvalidTransaction::BadProof));
 				}
 			} else {
