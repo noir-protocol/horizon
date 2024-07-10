398185c00ec818757f37e3f4e70a2b5273eb6654
code0xff <ian.jungyong.um@gmail.com>
2024-07-10 20:00:26 +0900

    refactor: Refactor chain id

--- frame/cosmos/types/src/sign_doc.rs
+++ frame/cosmos/types/src/sign_doc.rs
@@ -26,8 +26,10 @@ pub fn get_sign_doc_bytes(
 	account_number: u64,
 ) -> Result<[u8; 32], DecodeError> {
 	let tx = cosmrs::Tx::from_bytes(tx_bytes).map_err(|_| DecodeError::InvalidTxData)?;
-	let chain_id = core::str::from_utf8(chain_id).map_err(|_| DecodeError::InvalidChainId)?;
-	let chain_id = chain::Id::from_str(chain_id).map_err(|_| DecodeError::InvalidChainId)?;
+	let chain_id = core::str::from_utf8(chain_id)
+		.map(chain::Id::from_str)
+		.map_err(|_| DecodeError::InvalidChainId)?
+		.map_err(|_| DecodeError::InvalidChainId)?;
 	let sign_doc = cosmrs::tx::SignDoc::new(&tx.body, &tx.auth_info, &chain_id, account_number)
 		.map_err(|_| DecodeError::InvalidSignDoc)?;
 	let sign_doc_bytes = sign_doc.into_bytes().map_err(|_| DecodeError::InvalidSignDoc)?;
@@ -61,8 +63,7 @@ mod tests {
 		let tx_raw = "CpMBCpABChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEnAKLWNvc21vczFxZDY5bnV3ajk1Z3RhNGFramd5eHRqOXVqbXo0dzhlZG1xeXNxdxItY29zbW9zMWdtajJleGFnMDN0dGdhZnBya2RjM3Q4ODBncm1hOW53ZWZjZDJ3GhAKBXVhdG9tEgcxMDAwMDAwEnEKTgpGCh8vY29zbW9zLmNyeXB0by5zZWNwMjU2azEuUHViS2V5EiMKIQIKEJE0H+VmS/oXgtXgR3lokGjJFrBMs2XsMVN1VoTZoRIECgIIARIfChUKBXVhdG9tEgw4ODY4ODAwMDAwMDAQgMDxxZSVFBpA9+DRmMYoIcxYF8jpNfUjMIMB4pgZ9diC8ySbnhc6YU84AA3b/0RsCr+nx9AZ27FwcrKJM/yBh8lz+/A9BFn3bg==";
 
 		let tx_bytes = Base64::decode_vec(&tx_raw).unwrap();
-		let expected_hash =
-			get_sign_doc_bytes(&tx_bytes, b"theta-testnet-001", 754989u64).unwrap();
+		let expected_hash = get_sign_doc_bytes(&tx_bytes, b"theta-testnet-001", 754989u64).unwrap();
 
 		let sign_doc_raw = "CpMBCpABChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEnAKLWNvc21vczFxZDY5bnV3ajk1Z3RhNGFramd5eHRqOXVqbXo0dzhlZG1xeXNxdxItY29zbW9zMWdtajJleGFnMDN0dGdhZnBya2RjM3Q4ODBncm1hOW53ZWZjZDJ3GhAKBXVhdG9tEgcxMDAwMDAwEnEKTgpGCh8vY29zbW9zLmNyeXB0by5zZWNwMjU2azEuUHViS2V5EiMKIQIKEJE0H+VmS/oXgtXgR3lokGjJFrBMs2XsMVN1VoTZoRIECgIIARIfChUKBXVhdG9tEgw4ODY4ODAwMDAwMDAQgMDxxZSVFBoRdGhldGEtdGVzdG5ldC0wMDEgrYou";
 		let hash = sha2_256(&Base64::decode_vec(&sign_doc_raw).unwrap());
