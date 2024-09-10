b37ae9dc73d74d3ff687b1a6e6957eaeae0433c5
code0xff <ian.jungyong.um@gmail.com>
2024-09-10 14:45:29 +0900

    chore: Remove deny unused_crate_dependencies

--- frame/cosmos/rpc/src/lib.rs
+++ frame/cosmos/rpc/src/lib.rs
@@ -16,8 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#![deny(unused_crate_dependencies)]
-
 mod cosmos;
 
 pub use cosmos::{Cosmos, CosmosApiServer};
